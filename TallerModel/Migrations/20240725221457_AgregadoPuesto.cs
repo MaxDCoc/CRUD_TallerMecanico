dba8ec94eeb49d4e6246fd5fd46a1977a24e5c7f
MartinLSME08 <martinlopezsoto@gmail.com>
2024-07-26 16:42:27 -0300

    Primer commit

---
+++ TallerModel/Migrations/20240725221457_AgregadoPuesto.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TallerModel.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregadoPuesto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    UsuarioId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Apellido = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Puesto = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.UsuarioId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
---
+++ TallerModel/Model.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using static System.Net.Mime.MediaTypeNames;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using Microsoft.EntityFrameworkCore;
+namespace TallerModel
+{
+
+    public class TallerContext : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseInMemoryDatabase("TallerMecanicoDB");
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Taller;Trusted_Connection=True;");
+            //optionsBuilder.UseSqlServer(@"Server=LS_SERVERPR\SQLEXPRESS;Database=PruebaCarrito2;user=sa;password=VALE3618");
+            //optionsBuilder.UseSqlite("Data Source=CarritosCompraStandardLite.db");
+            //optionsBuilder.UseNpgsql("Host=localhost;Database=PruebaCarrito2;Username=postgres;Password=postgres");
+            //optionsBuilder.UseMySQL("server=localhost;database=library;user=user;password=password");
+        }
+
+    }
+
+    public enum Rango
+        {
+        Gerente,
+        Administrativo,
+        DT,
+        Mecanico
+        }
+
+
+    public class Usuario
+    {
+        public int UsuarioId { get; set; }
+        public string Apellido { get; set; } = string.Empty;
+        public string Nombre { get; set; } = string.Empty;
+
+        public Rango? Puesto { get; set; } 
+    }
+
+    public class UsuarioServices
+    {
+        private TallerContext _context;
+
+        public UsuarioServices()
+        {
+            _context = new TallerContext();
+        }
+
+        public Usuario Create(Usuario usuario)
+        {
+            _context.Usuarios.Add(usuario);
+            _context.SaveChanges();
+            return (usuario);
+        }
+
+        public IEnumerable<Usuario> GetAll()
+        {
+            var usuarios = _context.Usuarios.ToList();
+            return usuarios;
+        }
+
+        public Usuario? GetById(int id)
+        {
+            {
+                var usuario = _context.Usuarios.SingleOrDefault(u => u.UsuarioId == id);
+
+                if (usuario == null)
+                {
+                    return null;
+                }
+
+                return (usuario);
+            }
+        }
+
+        public IEnumerable<Usuario>? GetByNombreyAp(string aBuscar)
+        {
+            var usuarios = _context.Usuarios.Where(u => u.Nombre.Contains(aBuscar) || u.Apellido.Contains(aBuscar)).ToList();
+            
+            if (usuarios == null)
+            {
+                return null;
+            }
+
+            return usuarios;
+        }
+
+        public IEnumerable<Usuario>? GetByPuesto(Rango puestoABuscar)
+        {
+            var usuarios = _context.Usuarios.Where(u => u.Puesto == puestoABuscar).ToList();
+
+            if (usuarios == null)
+            {
+                return null;
+            }
+
+            return usuarios;
+        }
+
+        public Usuario Update(Usuario usuario)
+        {
+            _context.Usuarios.Update(usuario);
+            _context.SaveChanges();
+            return (usuario);
+        }
+
+        public int? Delete(int id)
+        {
+            var usuario = _context.Usuarios.Single(u => u.UsuarioId == id);
+            
+            if (usuario == null)
+            {
+                return null;
+            }
+
+            _context.Usuarios.Remove(usuario);
+            _context.SaveChanges();
+            return (id);
+        }
+    }
+
+}
